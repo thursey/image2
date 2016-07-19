6e959d5fb29debda7eb737780be4bee1a8835aee
thursey <thursey@sas.upenn.edu>
2016-07-19 10:40:35 -0400

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
